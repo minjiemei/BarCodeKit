db7f5c0fa296f8330fa07716c82874be0cb7aac1
minjiemei <minjie.mei@beesoft.io>
2025-07-01 13:18:23 +0800

    no message

--- Core/Source/BCKCode.m
+++ Core/Source/BCKCode.m
@@ -652,7 +652,9 @@ NSString * const BCKCodeDrawingIncludeCaption = @"BCKCodeDrawingIncludeCaption";
         default:
            x = CGRectGetMidX(rect) - width/2.0f;
     }
-    
+    if (x < 0) {
+        x = 0;
+    }
 	CGFloat y = CGRectGetMaxY(rect) - descent;
 	CGContextSetTextPosition(context, x, y);
 	
@@ -975,7 +977,7 @@ NSString * const BCKCodeDrawingIncludeCaption = @"BCKCodeDrawingIncludeCaption";
 			// Draw Captions
 			if (leftDigits)
 			{
-                [self _drawCaptionText:leftDigits fontName:fontName fontSize:optimalCaptionFontSize alignment:NSTextAlignmentRight inRect:leftNumberFrame context:context];
+                [self _drawCaptionText:leftDigits fontName:fontName fontSize:optimalCaptionFontSize alignment:NSTextAlignmentCenter inRect:leftNumberFrame context:context];
 			}
 			
 			if (rightDigits)
